73b5de30ed954e61343404bfe0110dd894fbcbae
amir95107 <a.janmohammaid@gmail.com>
2021-07-19 02:07:32 +0430

    First commit

---
+++ DataLayer/Models/Data.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Models
+{
+    public class Data
+    {
+        [Key]
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Family { get; set; }
+        [Required]
+        public int Age { get; set; }
+    }
+}
---
+++ Test/Controllers/DataController.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DataLayer.Models;
+
+namespace Test.Controllers
+{
+    public class DataController : Controller
+    {
+        private AsroonDB db = new AsroonDB();
+
+        // GET: Data
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Table()
+        {
+            return PartialView(db.Data.ToList());
+        }
+
+        // GET: Data/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Data data = db.Data.Find(id);
+            if (data == null)
+            {
+                return HttpNotFound();
+            }
+            return View(data);
+        }
+
+        // GET: Data/Create
+        public ActionResult Create()
+        {
+            return PartialView();
+        }
+
+        // POST: Data/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        public ActionResult Create([Bind(Include = "ID,Name,Family,Age")] Data data)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Data.Add(data);
+                db.SaveChanges();
+                return PartialView("Table");
+            }
+
+            return null;
+        }
+
+        [HttpPost]
+        public ActionResult CreateData(string Name, string Family,int Age)
+        {
+            Data data = new Data() { 
+                Name=Name,
+                Family=Family,
+                Age=Age
+            };
+
+            db.Data.Add(data);
+            db.SaveChanges();
+
+            return PartialView("Table",db.Data.ToList());
+        }
+
+        // GET: Data/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Data data = db.Data.Find(id);
+            if (data == null)
+            {
+                return HttpNotFound();
+            }
+            return View(data);
+        }
+
+        // POST: Data/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Name,Family,Age")] Data data)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(data).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(data);
+        }
+
+        // GET: Data/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Data data = db.Data.Find(id);
+            if (data == null)
+            {
+                return HttpNotFound();
+            }
+            return View(data);
+        }
+
+        // POST: Data/Delete/5
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Data data = db.Data.Find(id);
+            db.Data.Remove(data);
+            db.SaveChanges();
+            return PartialView("Table", db.Data.ToList());
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
